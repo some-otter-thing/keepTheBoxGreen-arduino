6161f07e6cae82c915ff5702a07436943deb0a18
Soyoon Choi <soyoon.choi@code.berlin>
2021-10-10 15:22:36 +0200

    add logic to main

--- Main/Main.ino
+++ Main/Main.ino
@@ -3,10 +3,12 @@
 
 #include "arduino_secrets.h"
 #include "main_constants.h"
+#include <DHT.h>
 
 char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
 int status = WL_IDLE_STATUS;
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup()
 {
@@ -22,6 +24,8 @@ void setup()
   Serial.println("You're connected to the network");
   printWiFiData();
 
+  // temperature and humdity
+  dht.begin();
   // ultrasonic
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
@@ -43,26 +47,31 @@ void loop()
   duration = pulseIn(echoPin, HIGH); // returns the sound wave travel time in microseconds
   distance = duration * 0.034 / 2;   // speed of sound wave divided by 2 (go and back)
 
-  sittingTimeColor = GetSittingTimeColor(distance, currentTime);
+  temp = dht.readTemperature();
+  hum = dht.readHumidity();
+  sittingTimeColor = getSittingTimeColor(distance, currentTime);
+  tempColor = getTempColor(temp);
+  humColor = getHumColor(hum);
 
-  if (sittingTimeColor == "GREEN")
+  if (sittingTimeColor == GREEN && tempColor == GREEN && humColor == GREEN)
   {
     digitalWrite(LED_PIN_1, HIGH);
     digitalWrite(LED_PIN_2, LOW);
     digitalWrite(LED_PIN_3, LOW);
   }
-  else if (sittingTimeColor == "YELLOW")
-  {
-    digitalWrite(LED_PIN_1, LOW);
-    digitalWrite(LED_PIN_2, HIGH);
-    digitalWrite(LED_PIN_3, LOW);
-  }
-  else if (sittingTimeColor == "RED")
+    else if (sittingTimeColor == RED || tempColor == RED || humColor == RED)
   {
     digitalWrite(LED_PIN_1, LOW);
     digitalWrite(LED_PIN_2, LOW);
     digitalWrite(LED_PIN_3, HIGH);
   }
+  else if (sittingTimeColor == YELLOW || tempColor == YELLOW || humColor == YELLOW)
+  {
+    digitalWrite(LED_PIN_1, LOW);
+    digitalWrite(LED_PIN_2, HIGH);
+    digitalWrite(LED_PIN_3, LOW);
+  }
+
 
   delay(2000);
 }
